18e0f110cd0fe7adf7675d72b8ef074c484959b4
yukihira-pot <r.magoshi.ma54@gmail.com>
2023-08-17 22:18:14 +0900

    短音ボタンと長音ボタンの配置

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,40 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import './App.css';
 
-function App() {
+interface PlaySoundProps {
+  value: string;
+  seconds: number;
+  frequency: number;
+}
+
+// 引数にある秒数だけ音を鳴らす
+export function PlaySound({ value, seconds, frequency }: PlaySoundProps) {
   const [audioCtx] = useState(new window.AudioContext());
-  const [oscillator, setOscillator] = useState<OscillatorNode | null>(null);
-  const [isPlaying, setIsPlaying] = useState(false);
-
-  useEffect(() => {
-    const playButton = document.getElementById("oscPlayer");
-    if (!playButton) return;
-
-    const handleMousedown = (event: MouseEvent) => {
-      if (!isPlaying) {
-        playButton.innerHTML = "⏸ Stop";
-        const osc = audioCtx.createOscillator();
-        if (!osc) return;
-        osc.type = 'square';
-        osc.frequency.setValueAtTime(440, audioCtx.currentTime);
-        osc.connect(audioCtx.destination);
-        osc.start();
-        setOscillator(osc);
-        setIsPlaying(true);
-      } else {
-        playButton.innerHTML = "▶️ Play";
-        if (oscillator) {
-          oscillator.stop();
-          oscillator.disconnect(audioCtx.destination);
-        }
-        setOscillator(null);
-        setIsPlaying(false);
-      }
-    };
-
-    playButton.addEventListener("mousedown", handleMousedown, false);
-
-    return () => {
-      playButton.removeEventListener("mousedown", handleMousedown);
-    };
-  }, [audioCtx, oscillator, isPlaying]);
+  const [_, setOscillator] = useState<OscillatorNode | null>(null);
 
 
+  const handleMousedown = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const osc = audioCtx.createOscillator();
+    if (!osc) return;
+    osc.type = 'square';
+    osc.frequency.setValueAtTime(frequency, audioCtx.currentTime);
+    osc.connect(audioCtx.destination);
+    osc.start();
+    setOscillator(osc);
+    setTimeout(() => {
+      osc.stop();
+      osc.disconnect(audioCtx.destination);
+    }, seconds * 1000);
+  }
+  const playButton = <button className="play__btn" onClick={handleMousedown}>▶️ {value}</button>;
+  return playButton;
+}
+
+function App() {
   return (
     <>
-      <button id="oscPlayer">▶️ Play</button>
+      <div><PlaySound value="短音" seconds={0.3} frequency={7040} /></div>
+      <div><PlaySound value="長音" seconds={0.6} frequency={7040} /></div>
     </>
   );
 }
