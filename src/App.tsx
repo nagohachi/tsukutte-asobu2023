cbdd2b957086aa372bb7a1b0b1bf031c15e4348b
yukihira-pot <r.magoshi.ma54@gmail.com>
2023-08-18 18:01:13 +0900

    テスト

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import FrequencyBar from "./ FrequencyBar";
 import MenuTabs from "./MenuTabs";
 import { PlaySoundForFreeTime, PlaySoundMusic } from "./PlaySound";
 import { frequency } from "./Params";
-import { TextToMorse } from "./TextToMorse";
+// import { TextToMorse } from "./TextToMorse";
 import { DoubleTouchThenSwipe } from "./Swipes";
 import { useState } from "react";
 import "./App.css";
@@ -20,18 +20,18 @@ function App() {
           <FrequencyBar />
           <div>2本指でタッチ & 上スワイプして電源偽装モードに切り替え</div>
           <MenuTabs />
-          <div>
+          <span>
             <PlaySoundForFreeTime
               value="押下の間ずっと"
               frequency={frequency}
             />
-          </div>
-          <div>
+          </span>
+          {/* <div>
             <TextToMorse text={"sos"} />
-          </div>
-          <div>
+          </div> */}
+          <span>
             <PlaySoundMusic value="妨害音声を再生" />
-          </div>
+          </span>
         </section>
       </div>
     </>
