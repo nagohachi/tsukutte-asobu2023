95c41ab8762057523a05820b094c909b8ccbbadb
yukihira-pot <r.magoshi.ma54@gmail.com>
2023-08-18 12:21:14 +0900

    vite.config.ts を修正

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,9 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // root: "./src",
+  // root: "./src", 今回は直下に index.html があるため不要
   build: {
-    outDir: "../dist",
+    outDir: "./dist",
   },
   base: "/tsukutte-asobu2023/",
   plugins: [react()],
