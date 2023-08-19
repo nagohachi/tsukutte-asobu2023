e0a905f0a4cb85fe40007dfae24b06f69e2a03d6
knknk98 <65712721+knknk98@users.noreply.github.com>
2023-08-19 14:43:02 +0900

    lpページを作成

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import Lp from "./Lp.tsx";
+import "./index.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/tsukutte-asobu2023" element={<App />} />
+        <Route path="/tsukutte-asobu2023/lp" element={<Lp />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
