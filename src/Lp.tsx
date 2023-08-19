d11e83ed084b0f47788804690f93d6029f6df7c4
knknk98 <65712721+knknk98@users.noreply.github.com>
2023-08-19 19:44:42 +0900

    ルーティング変更 (#38)

--- src/Lp.tsx
+++ src/Lp.tsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
-import Button from '@mui/material/Button';
-import GitHubIcon from '@mui/icons-material/GitHub';
+import Button from "@mui/material/Button";
+import GitHubIcon from "@mui/icons-material/GitHub";
 import iconSrc from "./assets/img/icon.png";
 import { createTheme, ThemeProvider } from "@mui/material";
 import { themeOptions } from "./ThemeOptions";
@@ -9,48 +9,65 @@ function Lp() {
   const theme = createTheme(themeOptions);
   return (
     <>
-      <div style={{ margin: '0 10%' }}>
+      <div style={{ margin: "0 10%" }}>
         <img
           style={{ display: "block", margin: "auto", width: "40%" }}
           src={iconSrc}
           alt="Application Name"
-        />        <h3>正々堂々じゃ、<br />立ち行かないこともある。<br />
-          ちょっぴりズルくて耳聡い、<br />そんな大人のカンニングアプリ。</h3>
-        <p style={{ margin: '0' }}>蚊ン忍具は、モールス信号とモスキート音を組み合わせた通信機能と、周囲の受験者への妨害機能を備えたカンニングアプリです。</p>
+        />{" "}
+        <h3>
+          正々堂々じゃ、
+          <br />
+          立ち行かないこともある。
+          <br />
+          ちょっぴりズルくて耳聡い、
+          <br />
+          そんな大人のカンニングアプリ。
+        </h3>
+        <p style={{ margin: "0" }}>
+          蚊ン忍具は、モールス信号とモスキート音を組み合わせた通信機能と、周囲の受験者への妨害機能を備えたカンニングアプリです。
+        </p>
         <ul>
-          <li>通信モード
+          <li>
+            通信モード
             <ul>
               <li>周波数を設定してモールス信号を発信する。</li>
               <li>入力したメッセージをモールス信号に変換して発信する。</li>
             </ul>
           </li>
-          <li>妨害モード
+          <li>
+            妨害モード
             <ul>
-              <li>様々な周波数のモスキート音や、ダミーのリスニング音声を再生する。</li>
+              <li>
+                様々な周波数のモスキート音や、ダミーのリスニング音声を再生する。
+              </li>
             </ul>
           </li>
-          <li>電源偽装モード
+          <li>
+            電源偽装モード
             <ul>
               <li>画面が真っ暗になり、最低限の操作で通信と妨害が行える。</li>
               *電源偽装モード使用時は端末をダークモードに設定してください。
             </ul>
           </li>
         </ul>
-        <p style={{ margin: '0' }}>遷移先のページをホーム画面に追加してご使用ください。</p>
-        <p style={{ margin: '0' }}>使い方の詳細は→
-          <Link to='https://github.com/yukihira-pot/tsukutte-asobu2023'>
+        <p style={{ margin: "0" }}>
+          遷移先のページをホーム画面に追加してご使用ください。
+        </p>
+        <p style={{ margin: "0" }}>
+          使い方の詳細は→
+          <Link to="https://github.com/yukihira-pot/tsukutte-asobu2023">
             <GitHubIcon></GitHubIcon>
           </Link>
         </p>
-
-        <div style={{ width: '100%', margin: '0 20%' }}>
+        <div style={{ width: "100%", margin: "0 20%" }}>
           <ThemeProvider theme={theme}>
-            <Link to="/tsukutte-asobu2023">
+            <Link to="/tsukutte-asobu2023/home">
               <Button variant="contained" href="#contained-buttons">
                 カンニングを始める
               </Button>
-            </Link></ThemeProvider>
-
+            </Link>
+          </ThemeProvider>
         </div>
       </div>
     </>
--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   // root: "./src", 今回は直下に index.html があるため不要
   build: {
-    outDir: "./dist",
+    outDir: "./dist"
   },
   base: "/tsukutte-asobu2023/",
   plugins: [
@@ -18,8 +18,8 @@ export default defineConfig({
         theme_color: "#000000",
         background_color: "#000000",
         display: "fullscreen",
-        scope: "/tsukutte-asobu2023/",
-        start_url: "/tsukutte-asobu2023/lp/",
+        scope: "/tsukutte-asobu2023/home/",
+        start_url: "/tsukutte-asobu2023/home/",
         name: "\u868a\u30f3\u5fcd\u5177",
         description:
           "\u30e2\u30fc\u30eb\u30b9\u4fe1\u53f7\u3068\u30e2\u30b9\u30ad\u30fc\u30c8\u97f3\u3092\u7d44\u307f\u5408\u308f\u305b\u305f\u901a\u4fe1\u6a5f\u80fd\u3068\u3001\u5468\u56f2\u306e\u53d7\u9a13\u8005\u3078\u306e\u59a8\u5bb3\u6a5f\u80fd\u3092\u5099\u3048\u305f\u30ab\u30f3\u30cb\u30f3\u30b0\u30a2\u30d7\u30ea",
@@ -28,26 +28,26 @@ export default defineConfig({
           {
             src: "/icon-192x192.png",
             sizes: "192x192",
-            type: "image/png",
+            type: "image/png"
           },
           {
             src: "/icon-256x256.png",
             sizes: "256x256",
-            type: "image/png",
+            type: "image/png"
           },
           {
             src: "/icon-384x384.png",
             sizes: "384x384",
-            type: "image/png",
+            type: "image/png"
           },
           {
             src: "/icon-512x512.png",
             sizes: "512x512",
             type: "image/png",
-            purpose: "any maskable",
-          },
-        ],
-      },
-    }),
-  ],
+            purpose: "any maskable"
+          }
+        ]
+      }
+    })
+  ]
 });
