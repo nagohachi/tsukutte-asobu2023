149d5d07ef224db4af8650055f39cd158af1ea68
knknk98 <65712721+knknk98@users.noreply.github.com>
2023-08-19 17:48:14 +0900

    タイトル変更 (#33)

--- vite.config.ts
+++ vite.config.ts
@@ -13,12 +13,38 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: "autoUpdate",
+      includeAssets: ["apple-touch-icon.png"],
       manifest: {
+        theme_color: "#8E2A66",
+        background_color: "#8E2A66",
+        display: "fullscreen",
+        scope: "/tsukutte-asobu2023/",
+        start_url: "/tsukutte-asobu2023/",
+        name: "\u868a\u30f3\u5fcd\u5177",
+        description:
+          "\u30e2\u30fc\u30eb\u30b9\u4fe1\u53f7\u3068\u30e2\u30b9\u30ad\u30fc\u30c8\u97f3\u3092\u7d44\u307f\u5408\u308f\u305b\u305f\u901a\u4fe1\u6a5f\u80fd\u3068\u3001\u5468\u56f2\u306e\u53d7\u9a13\u8005\u3078\u306e\u59a8\u5bb3\u6a5f\u80fd\u3092\u5099\u3048\u305f\u30ab\u30f3\u30cb\u30f3\u30b0\u30a2\u30d7\u30ea",
+        short_name: "\u868a\u30f3\u5fcd\u5177",
         icons: [
           {
-            src: "/pwaicon.png",
-            sizes: "144x144",
+            src: "/icon-192x192.png",
+            sizes: "192x192",
             type: "image/png"
+          },
+          {
+            src: "/icon-256x256.png",
+            sizes: "256x256",
+            type: "image/png"
+          },
+          {
+            src: "/icon-384x384.png",
+            sizes: "384x384",
+            type: "image/png"
+          },
+          {
+            src: "/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable"
           }
         ]
       }
